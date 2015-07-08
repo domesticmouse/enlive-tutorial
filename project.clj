8ac473349a63327ad3dd558611e635358d001e24
Brett Morgan <brett.morgan@gmail.com>
2015-07-08 22:17:10 +1000

    Updating to Clojure 1.7.0 et al

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject enlive-tutorial "0.1.0"
   :description "Enlive Tutorial"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [enlive "1.1.1"]
-                 [ring "1.2.0"]
-                 [net.cgrand/moustache "1.1.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [enlive "1.1.5"]
+                 [ring "1.4.0-RC2"]
+                 [net.cgrand/moustache "1.2.0-alpha2"]])
